82c239672af14dbc70eb82d6079e6bf888b8faa3
Pranay <pranaypandeyofficial@gmail.com>
2023-12-27 11:48:14 +0530

    Style: Add selected state and header name

--- src/app/components/menu-item.tsx
+++ src/app/components/menu-item.tsx
@@ -1,11 +1,14 @@
+'use client';
 import React from "react";
 import { IconType } from "react-icons";
 
-const MenuItem = (props: { name: string; icon: any}) => {
+const MenuItem = (props: { name: string; icon: any, selected: boolean, onClick: ()=>void}) => {
   
-   
   return (<>
-      <li className="font-semibold text-base border-0 border-b-2 border-slate-200 w-[120%] border-solid flex gap-4 items-center cursor-pointer hover:text-red-600">
+      <li className= {props.selected ? "font-semibold text-base border-0 border-b-2 border-slate-200 w-[120%] border-solid flex gap-4 items-center cursor-pointer text-red-500" : 
+      "font-semibold text-base border-0 border-b-2 border-slate-200 w-[120%] border-solid flex gap-4 items-center cursor-pointer"}
+        onClick={props.onClick}
+      >
         {props.icon} <p>{props.name}</p></li>
   </>);
 };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,8 @@
-import React from "react";
+'use client';
+import React, {useState} from "react";
 import MenuItem from "../components/menu-item";
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { Menu } from "@mui/material";
 import { FaHome } from "react-icons/fa";
 import { IoPersonSharp } from "react-icons/io5";
 import { FaListUl } from "react-icons/fa";
@@ -12,24 +10,29 @@ import { MdWork } from "react-icons/md";
 import { IoChatbubblesSharp } from "react-icons/io5";
 
 const MenuContents = [
-  { name: "Home" , icon: <FaHome/>},
-  { name: "About" , icon: <IoPersonSharp/>},
-  { name: "Services" , icon: <FaListUl/>},
-  { name: "Experience" , icon: <MdWork/>},
-  { name: "Contact" , icon: <IoChatbubblesSharp/>}
+  { name: "Home" , icon: <FaHome/>, selected: true},
+  { name: "About" , icon: <IoPersonSharp/>, selected: false},
+  { name: "Services" , icon: <FaListUl/>, selected: false},
+  { name: "Experience" , icon: <MdWork/>, selected: false},
+  { name: "Contact" , icon: <IoChatbubblesSharp/>, selected: false}
 ];
 
 const Page = () => {
+  const [selected, setSelected] = useState<string>('Home');
   return (
     <>
       <Box>
         < Grid container spacing={2} >
           <Grid item xs={3}>
             <div className="h-screen flex flex-col align-center justify-between">
-              <h1 className=""></h1>
+              <div className="relative mx-auto mt-16">
+                <div className="absolute top-0 right-0 w-4 h-4 border-l-0 border-b-0 border-t-[6px] border-r-[6px] border-red-500 border-solid"></div>
+                <h1 className="text-center px-5 py-2 text-3xl">Pranay</h1>
+                <div className="absolute bottom-0 left-0 w-4 h-4 border-l-[6px] border-b-[6px] border-t-0 border-r-0 border-red-500 border-solid"></div>
+              </div>
               <ul className="m-auto flex flex-col gap-7">
               {MenuContents.map((item, index) => (
-                <MenuItem key={index} name={item.name} icon={item.icon}/>
+                <MenuItem key={index} name={item.name} icon={item.icon} selected={selected===item.name} onClick={()=>setSelected(item.name) }/>
               )
               )}
               </ul>
